b575db5b28a34ddca699bc0baaf27cb8ba70b491
Arman <keremeris20@gmail.com>
2022-09-11 17:54:00 +0400

    aa

--- api/signup.js
+++ api/signup.js
@@ -2,6 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const admin = require("firebase-admin");
+
+const credentials = require("../jlsampleproject-firebase-adminsdk-spjvo-13c7ec3190.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(credentials)
+});
 router.post("/", async (req, res) => {
     console.log(req.body);
     const user ={
